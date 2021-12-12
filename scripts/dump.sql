69db260f3c9c277736f0e498b24b6fc05ba71a79
gjonusys <93316918+gjonusys@users.noreply.github.com>
2021-12-12 23:22:58 +0200

    Update dump.sql

--- scripts/dump.sql
+++ scripts/dump.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS data (
   name varchar(255),
-  number varchar(12) PRIMARY_KEY
+  number varchar(12) PRIMARY KEY
   );
     INSERT INTO data VALUES('Emilis', '+37011111111');
     INSERT INTO data VALUES('Julius', '+37011111114');
     INSERT INTO data VALUES('Deividas', '+37011111113');
-    INSERT INTO data VALUES('Gyts', '+37011111112');
+    INSERT INTO data VALUES('Gytis', '+37011111112');
 ALTER TABLE data OWNER TO "postgres";
