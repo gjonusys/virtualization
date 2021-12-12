14a39277f7a884510d25b406b1220dc75c250528
Valma Julius <julius.valma@mif.stud.vu.lt>
2021-12-12 20:36:22 +0200

    dd

--- test.php
+++ test.php
@@ -3,6 +3,12 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <p>
+  <?php 
+     $db_connection = pg_connect("host=10.0.1.243 dbname=postgres user=postgres password=1234");
+     $result = pg_query($db_connection, "SELECT * FROM data");
+     echo var_dump($result);
+  ?>
+  </p>
  </body>
 </html>
