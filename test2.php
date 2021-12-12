54275051f425884e39324b72ed954668d169289b
Valma Julius <julius.valma@mif.stud.vu.lt>
2021-12-12 21:27:36 +0200

    Update test2.php

--- test2.php
+++ test2.php
@@ -8,18 +8,22 @@
 <body>
 <p>
 <?php
-$conn = pg_connect("host=10.0.1.243 port=5432 dbname='postgres' user='postgres' password='1234'");
+
+$conn = pg_connect("host=10.0.1.243 port=5432 dbname=postgres user=postgres password=1234");
 if (!$conn) {
-    echo "An error occurred.\n";
-    exit;
-   }
-   $result = pg_query($conn, "SELECT * FROM data");
-   if (!$result) {
-    echo "An error occurred.\n";
-    exit;
-   }
- echo 'xui';
-?>
+  echo "An error occurred.\n";
+  exit;
+}
+
+$result = pg_query($conn, "SELECT * FROM data");
+if (!$result) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+echo $result;
+echo 'LOX';
+?> 
 </p>
 
 </body>
